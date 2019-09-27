d7dfef1f7ac66b3672f1237b472c734bef753935
Lorena Perales <lperales321@gmail.com>
2019-09-26 22:26:05 -0500

    Calculate scores, write and reset

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -46,10 +46,16 @@ $(document).ready(function() {
             ties: 0
         };
 
+        if (player === 1) {
+            $("#instructions").text('Waiting for Player 2...');
+        }
+        else {
+            writeScores();
+        }
+
         players.push(newPlayer);
 
         saveToFirebase(newPlayer);
-        //$("#instructions").text(`Name: ${name}   Player ${currentPlayer}`);
     });
         
     function saveToFirebase(player) {
@@ -73,12 +79,14 @@ $(document).ready(function() {
     $("#rock-btn").on("click", function() {
         event.preventDefault();
 
+        currentPlayer = getCurrentPlayer();
+
         //Write selection
         players[currentPlayer].selection = "r";
         console.log(`Player: ${players[currentPlayer].player}   Selection: ${players[currentPlayer].selection}`);
         
-        let winner = getWinner();
-        saveScore(winner);
+        processSelection(currentPlayer)
+        
         //$("#instructions").text(`Name: ${name}   Player ${currentPlayer}`);
     });
 
@@ -86,12 +94,14 @@ $(document).ready(function() {
     $("#paper-btn").on("click", function() {
         event.preventDefault();
 
+        currentPlayer = getCurrentPlayer();
+
         //Write selection
         players[currentPlayer].selection = "p";
         console.log(`Player: ${players[currentPlayer].player}   Selection: ${players[currentPlayer].selection}`);
-        
-        let winner = getWinner();
-        saveScore(winner);
+
+        processSelection(currentPlayer)
+
         //$("#instructions").text(`Name: ${name}   Player ${currentPlayer}`);
     });
 
@@ -99,22 +109,47 @@ $(document).ready(function() {
     $("#scissors-btn").on("click", function() {
         event.preventDefault();
 
+        currentPlayer = getCurrentPlayer();
+
         //Write selection
         players[currentPlayer].selection = "s";
         console.log(`Player: ${players[currentPlayer].player}   Selection: ${players[currentPlayer].selection}`);
         
-        let winner = getWinner();
-        saveScore(winner);
+        processSelection(currentPlayer)
+
         //$("#instructions").text(`Name: ${name}   Player ${currentPlayer}`);
     });
+
+    function processSelection(currentPlayer) {
+        let winner = getWinner();
+        saveToFirebase(players[currentPlayer]);
+
+        if (winner === -1) {
+            $("#instructions").text('Ready to Play Player 2');
+        }
+        else {
+            saveScore(winner);
+            writeScores();
+            resetScores();
+            $("#instructions").text('Ready to Play Player 1');
+        }
+    }
+
+    function getCurrentPlayer() {
+        if (players[1].selection === "") {
+            return 1;
+        }
+        else if (players[2].selection === "") {
+            return 2;
+        }
+    }
 
     function getWinner() {
         let winner = -1;
 
-        // if (player1Selection === "" || player2Selection === "") {
-        //     //waiting for response
-        players[2].selection = "r"
-        // };
+        if (players[1].selection === "" || players[2].selection === "") {
+            return winner;
+        };
 
         switch(players[1].selection) {
             case "r":
@@ -176,6 +211,44 @@ $(document).ready(function() {
         saveToFirebase(players[2]);
     }
 
+    function writeScores() {
+        const name = $('<h5>');
+        name.html(`Name: ${players[1].name}`);
+        const wins = $('<h5>');
+        wins.html(`Wins: ${players[1].wins}`);
+        const losses = $('<h5>');
+        losses.html(`Losses: ${players[1].losses}`);
+        const ties = $('<h5>');
+        ties.html(`Ties: ${players[1].ties}`);
+
+        $('#player-1').append(name);
+        $('#player-1').append(wins);
+        $('#player-1').append(losses);
+        $('#player-1').append(ties);
+
+        const name2 = $('<h5>');
+        name2.html(`Name: ${players[2].name}`);
+        const wins2 = $('<h5>');
+        wins2.html(`Wins: ${players[2].wins}`);
+        const losses2 = $('<h5>');
+        losses2.html(`Losses: ${players[2].losses}`);
+        const ties2 = $('<h5>');
+        ties2.html(`Ties: ${players[2].ties}`);
+
+        $('#player-2').append(name2);
+        $('#player-2').append(wins2);
+        $('#player-2').append(losses2);
+        $('#player-2').append(ties2);
+    }
+
+    function resetScores() {
+        players[1].selection = "";
+        saveToFirebase(players[1]);
+
+        players[2].selection = "";
+        saveToFirebase(players[2]);
+    }
+
     // Using .on("value", function(snapshot)) syntax will retrieve the data
     // from the database (both initially and every time something changes)
     // This will then store the data inside the variable "snapshot". We could rename "snapshot" to anything.
@@ -205,10 +278,18 @@ $(document).ready(function() {
                     ties: childData.ties
                 });
             });
+
+            //enable rps buttons
+            if (players.length === 3) {
+                $('#rock-btn').removeAttr("disabled")
+                $('#paper-btn').removeAttr("disabled")
+                $('#scissors-btn').removeAttr("disabled")
+                $("#instructions").text('Ready to Play Player 1');
+            }
         }
 
-        //Catch error
-        }, function(errorObject) {
+    //Catch error
+    }, function(errorObject) {
 
         // In case of error this will print the error
         console.log("The read failed: " + errorObject.code);
